c79221784a523e7f838f3ae0ad39d373fc335000
calebntende <132020839+calebntende@users.noreply.github.com>
2023-07-03 14:02:29 -0500

    Update Project2.cpp

--- Project2.cpp
+++ Project2.cpp
@@ -35,15 +35,12 @@ string getName(string&  name)
             a= name.at(i);
             while(!isalpha(a)&&!isspace(a))
             {
-                
                 cout<<"enter a valid username"<<endl;
                 getline(cin,name);
                 for(int i = 0 ; i<name.length(); i++)
                 {
                     a= name.at(i);
                 }
-                
-                
             }
         }
     return name;
@@ -55,25 +52,20 @@ void genShowMatix(int matrix[ROWS][COLUMNS]);
 
 void  genHideMatrix(int& lower_bound , int& upper_bound,int matrix2[ROWS][COLUMNS])
 {
-   
-    
+
     lower_bound = (rand()%100)+100;
     upper_bound = (rand()%100)+200;
    cout<<endl;
-    
-   // cout << lower_bound << " " << upper_bound<<endl;
-    
-   // int matrix[ROW_SIZE][COLUMN_SIZE];
     
    for(int i = 0 ; i < ROWS; i++)
     {
         for(int j =0; j < COLUMNS; j++)
         {
-            
             matrix2[i][j] = (rand()%((upper_bound-lower_bound+1)))+lower_bound;
         }
     }
     cout <<endl;
+    /*
     for(int i = 0 ; i <ROWS; i++)
     {
         for(int j =0; j <COLUMNS; j++)
@@ -83,6 +75,7 @@ void  genHideMatrix(int& lower_bound , int& upper_bound,int matrix2[ROWS][COLUMN
         cout <<endl;
     }
     cout <<endl;
+     */
 }
 void initialization (int& lower_bound , int& upper_bound,int matrix2[ROWS][COLUMNS]);
 
@@ -115,8 +108,7 @@ int main(int argc, const char * argv[])
     
     cout<<"enter you choice\n";
     cin >>gamechoice;
-    
-   
+
      genHideMatrix(lower_bound, upper_bound,matrix2);
     
     while(points >0)
@@ -136,18 +128,9 @@ int main(int argc, const char * argv[])
                 points--;
                 break;
             case Guess:
-            
-              // cout<< "guess a number\n";
-            //    cin >>guess1;
-                
-                
-                
-                
                 eliminate(guess1,matrix,matrix2);
                 guess(guess1, points, int1, int2, matrix2);
-                
-                                
-                
+         
                 break;
             case Reset:
                 
@@ -155,7 +138,8 @@ int main(int argc, const char * argv[])
                 points--;
                 break;
             case Exit:
-                
+                cout << "bye " << name <<endl;
+                cout << " final points are " << points<<endl;
                 
                 exit(0);
                 break;
@@ -184,16 +168,12 @@ int main(int argc, const char * argv[])
         }
  cout <<" enter what game option you want\n displayLeft = 1 \n displayRight = 2 \n Guess = 3\n Change = 4\n or Exit = 5 \n";
                 cout << int1 << " " << int2<<endl;
-        
-        
-        if(points)
-        
+
                 cout<<"updated points balance " << points <<" " <<endl;
                         cout<<"enter your choice ";
                         cin >> gamechoice;
     
-       
-        
+ 
         while(gamechoice ==1 &&(int2!=-1))
         {
             cout <<"you have already used your upperbound enter a valid game choice\n";
@@ -204,7 +184,6 @@ int main(int argc, const char * argv[])
             cout <<"you have already used your upperbound enter a valid game choice\n";
             cin >> gamechoice;
         }
-    
     }
  
     cout << "thank you for playing the game"<<endl;
@@ -307,6 +286,7 @@ void initialization (int& lower_bound , int& upper_bound, int matrix2[ROWS][COLU
         }
     }
     cout <<endl;
+    /*
     for(int i = 0 ; i <ROWS; i++)
     {
         for(int j =0; j <COLUMNS; j++)
@@ -316,12 +296,16 @@ void initialization (int& lower_bound , int& upper_bound, int matrix2[ROWS][COLU
         cout <<endl;
     }
     cout <<endl;
+     */
+     
 }
 
 bool allZeros(int matrix[][COLUMNS])
 {
-    for (int i = 0; i < ROWS; i++) {
-            for (int j = 0; j < COLUMNS; j++) {
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLUMNS; j++)
+            {
                 if (matrix[i][j] != 0)
                 {
                     return false; // Found a non-zero element, return false
@@ -343,7 +327,7 @@ int guess(int& guess1,int& points,int&int1, int& int2, int matrix2[][COLUMNS])
                              if(guess1 == matrix2[i][j]&& (int1==-1&&int2==-1))
                              {
                                  points+=5;
-                                 cout<<"You guessed correctly. You get 5 points."<<endl;
+                                 cout<<"You guessed correctly and have not used any bounds. +5 points"<<endl;
                                 return points;
                                  break;
                              }
@@ -359,7 +343,7 @@ int guess(int& guess1,int& points,int&int1, int& int2, int matrix2[][COLUMNS])
                              {
                                 // cout <<" you guessed correctly and you have not used any bounds\n";
                                  points+=1;
-                                 cout<<"You guessed correctly. You get 1 point. "<<endl;
+                                 cout<<"You guessed correctly. +1 will be added to your points "<<endl;
                                  return points;
                                  break;
                              }
@@ -377,7 +361,7 @@ int guess(int& guess1,int& points,int&int1, int& int2, int matrix2[][COLUMNS])
                              {
                                 // cout <<" you guessed correctly and you have not used any bounds\n";
                                  points-=10;
-                                 cout<<"You guessed incorrectly. You lose 10 points. "<<endl;
+                                 cout<<"You guessed incorrectly and used an upper or lower bound. -10 points "<<endl;
                                 return points;
                                  break;
                              }
@@ -406,7 +390,6 @@ int guess(int& guess1,int& points,int&int1, int& int2, int matrix2[][COLUMNS])
                      }
     return  points;
 }
-
 
 void setdisplayLeft(int&int1, int& lower_bound)
 {
